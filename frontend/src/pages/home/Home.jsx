b2401dd9d4b0a25637a69f79418b0569068c39bb
Sameer-khan-786 <85848440+Sameer-khan-786@users.noreply.github.com>
2024-09-15 05:24:24 -0400

    made site responsive

--- frontend/src/pages/home/Home.jsx
+++ frontend/src/pages/home/Home.jsx
@@ -1,15 +1,18 @@
+import React from 'react';
 import MessageContainer from "../../components/messages/MessageContainer";
-import Sidebar from "../../components/sidebar/Sidebar";
-
+import Sidebar from "../../components/sidebar/SideBar";
+import BackToTop from "../../components/BackToTop"; // Adjust the import path based on your directory structure
 
 const Home = () => {
-	return (
-		<div className="bg-white">
-		<div className='flex sm:h-[450px] md:h-[640px]  rounded-lg overflow-hidden bg-gray-400 bg-clip-padding backdrop-filter backdrop-blur-lg bg-opacity-0'>
-			<Sidebar />
-			<MessageContainer />
-		</div>
-		</div>
-	);
+  return (
+    <div className="bg-white">
+      <div className="flex flex-col md:flex-row h-full sm:h-[450px] md:h-[640px] rounded-lg overflow-hidden bg-gray-400 bg-clip-padding backdrop-filter backdrop-blur-lg bg-opacity-0">
+        <Sidebar />
+        <MessageContainer />
+      </div>
+      <BackToTop />
+    </div>
+  );
 };
+
 export default Home;
